a2f8199746b1c5f935b36dac2e0301c8ccf28465
simopopponen <simo.popponen@student.saimia.fi>
2017-02-15 08:42:30 +0200

    Added if-nro files

---
+++ task-if-nro1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_if_nro1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type number: ");
+            int iNro = int.Parse(Console.ReadLine());
+            if (iNro < 0)
+            {
+                Console.WriteLine("You type {0} and it is negative", iNro);
+            }
+            else if (iNro == 0)
+            {
+                Console.WriteLine("You type {0} and it is zero", iNro);
+            }
+            else
+            {
+                Console.WriteLine("You type {0} and it is positive", iNro);
+            }
+            Console.ReadLine();
+        }
+       
+    }
+}
